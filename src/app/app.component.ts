2ab5b3986f4581367e7db6313c9ae8817c2afe82
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-09-16 23:39:40 +0530

    assignment for template forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {
+    console.log('testing');
+  }
+  defaultSubscriptions = 'adv';
+  subscriptionsList = [
+    {
+      key: 'bsc',
+      value: 'Basic'
+    },
+    {
+      key: 'adv',
+      value: 'Advanced'
+    },
+    {
+      key: 'pro',
+      value: 'Professional'
+    }
+  ];
 
+  @ViewChild('f')
+  form: NgForm;
+  formData = {
+    email: '',
+    subscriptions: '',
+    password: ''
+  };
+
+  public ngOnInit() {
+    //  this.defaultsubscription
+  }
+
+
+  public onSubmit() {
+    this.formData.email = this.form.value.email;
+    this.formData.subscriptions = this.form.value.subscriptions;
+    this.formData.password = this.form.value.password;
+  }
 }
